33e5d6f3c895d03e16b555257e4cfa0325ea5ebe
dinushchathurya <dinushchathurya21@gmail.com>
2020-05-09 20:48:42 +0530

    Initialize the Package

---
+++ index.js
@@ -0,0 +1,20 @@
+var provinces = require('./data/provinces').provinces;
+
+//Get Districts Based On Province Input
+exports.getDistricts = function (province) {
+    province = province.trim().toUpperCase();
+    if (!provinces[province]) {
+        return null;
+    } else {
+        return provinces[province].districts;
+    }
+};
+
+//Get All Provinces
+exports.getProvinces = function () {
+    if (provinces) {
+        return Object.keys(provinces);
+    } else {
+        return null;
+    }
+};
