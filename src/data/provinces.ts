bb1302405e0ca5e534a5f4830785cc7df522737f
Dinush Chathurya <dinushchathurya21@gmail.com>
2025-05-12 12:32:38 +0100

    :hammer: Move data directory from root to src directory

--- src/data/provinces.ts
+++ src/data/provinces.ts
@@ -1,82 +1,16 @@
-exports.provinces = {
-    "Eastern":
-        {
-        "district":
-            [
-                "Ampara", 
-                "Batticaloa", 
-                "Trincomalee", 
-            ]
-        },
-    "North Central":
-        {
-        "district":
-            [
-                "Anuradhapura",
-                "Polonnaruwa",
+import { Province } from '../types';
 
-            ]
-        },
-    "Uva":
-    {
-        "district":
-            [
-                "Badulla",
-                "Moneragala",
-            ]
-    },
-    "Western":
-    {
-        "district":
-            [
-                "Colombo",
-                "Gampaha",
-                "Kalutara",
-            ]
-    },
-    "Southern":
-    {
-        "district":
-            [
-                "Galle",
-                "Hambantota",
-                "Matara",
-            ]
-    },
-    "Northern":
-    {
-        "district":
-            [
-                "Jaffna",
-                "Kilinochchi",
-                "Mannar",
-                "Mullativu",
-                "Vavuniya",
-            ]
-    },
-    "Central":
-    {
-        "district":
-            [
-                "Kandy",
-                "Matale",
-                "Nuwara Eliya",
-            ]
-    },
-    "Sabaragamuwa":
-    {
-        "district":
-            [
-                "Kegalle",
-                "Rathnapura",
-            ]
-    },
-    "North Western":
-    {
-        "district":
-            [
-                "Kurunegala",
-                "Puttalam",
-            ]
-    } 
-}
+/**
+ * Array of all Sri Lankan provinces
+ */
+export const provinces: Province[] = [
+    { id: 'western', name: 'Western', code: 'WP' },
+    { id: 'central', name: 'Central', code: 'CP' },
+    { id: 'southern', name: 'Southern', code: 'SP' },
+    { id: 'northern', name: 'Northern', code: 'NP' },
+    { id: 'eastern', name: 'Eastern', code: 'EP' },
+    { id: 'north-western', name: 'North Western', code: 'NW' },
+    { id: 'north-central', name: 'North Central', code: 'NC' },
+    { id: 'uva', name: 'Uva', code: 'UP' },
+    { id: 'sabaragamuwa', name: 'Sabaragamuwa', code: 'SG' },
+];
